81561143120e7927e7bc68e742bd53050fa25091
chauncey <chauncey1129@163.com>
2024-10-10 20:10:13 +0800

    完成用户列表接口

--- src/main/java/com/chauncey/springbootmybatis/controller/AdminController.java
+++ src/main/java/com/chauncey/springbootmybatis/controller/AdminController.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
@@ -21,7 +18,7 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
-    @GetMapping("/getUserList")
+    @PostMapping("/getUserList")
     @Operation(summary = "获取用户列表")
     public Result<PageBean<User>> getUserList(@RequestBody @Validated UserList userList) {
         PageBean<User> user = adminService.getUserList(userList);
